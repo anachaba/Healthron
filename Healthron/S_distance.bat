1d82bad4e52c6822f03f1e893b32b5b993213085
anach <54176002+codeZenon@users.noreply.github.com>
2023-01-12 13:16:49 +0000

    Update S_distance.bat

--- Healthron/S_distance.bat
+++ Healthron/S_distance.bat
@@ -3,3 +3,4 @@
 rem %SystemRoot%\explorer.exe "C:\Program Files\Innovation projects\CovidEnforcement\MaskCheck\Healthron\ZMask.py"
 python "C:\Program Files\Innovation projects\CovidEnforcement\MaskCheck\Healthron\ZDistance.py" %*
 pause
+Rem %replace with file path%
